0474619e8d672b152c49ec61b9835f56835dfcef
Maruen Mehana <maruen@gmail.com>
2016-04-01 15:07:35 -0300

    Some Adjustments

--- register.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-LINE=$1
-CODE=$2
-CMMD=`./yowsup-cli registration --register ${CODE} --phone 55${LINE} --cc 55`
--- requestcodeOI.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-LINE=$1
-CMMD=`./yowsup-cli registration --requestcode sms --phone 55${LINE} --cc 55 --mcc 724 --mnc 30`
