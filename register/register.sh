ab29775a21a710d498775ef1df73fd9f3978b702
Maruen Mehana <maruen@gmail.com>
2016-04-01 18:34:25 +0000

    Adjustments

--- register/register.sh
+++ register/register.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 LINE=$1
 CODE=$2
-CMMD=`./yowsup-cli registration --register ${CODE} --phone 55${LINE} --cc 55`
+CMMD=`${YOWSUP_HOME}/yowsup-cli registration --register ${CODE} --phone 55${LINE} --cc 55`
--- register/requestcodeCLARO.sh
+++ register/requestcodeCLARO.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 LINE=$1
-CMMD=`./yowsup-cli registration --requestcode sms --phone 55${LINE} --cc 55 --mcc 724 --mnc 05`
+CMMD=`${YOWSUP_HOME}/yowsup-cli registration --requestcode sms --phone 55${LINE} --cc 55 --mcc 724 --mnc 05`
