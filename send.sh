d5c3e533d2ae7aed39ba659e2f32108f212f7a81
Maruen Mehana <maruen@gmail.com>
2016-04-01 12:37:32 -0300

    Fixings

--- send.sh
+++ send.sh
@@ -2,4 +2,4 @@
 LINE=$1
 NUMBER=$2
 MESSAGE=$3
-CMMD=`/home/dongles/Local/yowsup.patched/yowsup-cli demos -M -c /home/dongles/Local/yowsup.patched/configs/${LINE}.cfg -s ${NUMBER} "${MESSAGE}"`
+`/home/dongles/Local/yowsup.patched/yowsup-cli demos -M -c /home/dongles/Local/yowsup.patched/configs/${LINE}.cfg -s ${NUMBER} "${MESSAGE}"`
